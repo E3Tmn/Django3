3cd14f3caf69575115ae5a4401f9ced9be345954
E3Tmn <100991827+E3Tmn@users.noreply.github.com>
2023-09-26 11:39:00 +0300

    Update script.py after review

--- script.py
+++ script.py
@@ -1,20 +1,33 @@
 from datacenter.models import Mark, Lesson, Chastisement, Schoolkid, Commendation
 import random
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
-def fix_marks(schoolkid):
-    child_marks = Mark.objects.filter(schoolkid = schoolkid, points__in = [2,3]) 
-    for child_mark in child_marks:
-        child_mark.points = 5
-        child_mark.save()
 
-def remove_chastisements(schoolkid): 
-    notes = Chastisement.objects.filter(schoolkid = schoolkid) 
+LAUDATORY_COMMENTS = ['Молодец!', 'Ты меня очень обрадовал!', 'С каждым разом у тебя получается всё лучше!', 'Я вижу, как ты стараешься!']
+
+
+def get_schoolkid(name):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=name)
+    except ObjectDoesNotExist:
+        return 'Does Not Exist!'
+    except MultipleObjectsReturned:
+        return 'Multiple Objects Returned!'
+    return schoolkid
+
+
+def fix_marks(name):
+    child_marks = Mark.objects.filter(schoolkid=get_schoolkid(name), points__in=[2, 3])
+    child_marks.update(points=5)
+
+
+def remove_chastisements(name):
+    notes = Chastisement.objects.filter(schoolkid=get_schoolkid(name))
     notes.delete()
 
 
-def create_commendation(name, les):
-    lst = ['Молодец!','Ты меня очень обрадовал!','С каждым разом у тебя получается всё лучше!','Я вижу, как ты стараешься!']
-    text = random.choice(lst)                                                                                                                               
-    schoolkid = Schoolkid.objects.get(full_name__contains = name)
-    last_lesson_6a = Lesson.objects.filter(year_of_study = 6, group_letter = 'А', subject__title = les).order_by('-date').first()
-    Commendation.objects.create(text = text, created = last_lesson_6a.date, schoolkid = schoolkid[0], subject = last_lesson_6a.subject, teacher = last_lesson_6a.teacher) 
+def create_commendation(name, lesson):
+    text = random.choice(LAUDATORY_COMMENTS)
+    schoolkid = get_schoolkid(name)
+    last_lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter, subject__title=lesson).order_by('-date').first()
+    Commendation.objects.create(text=text, created=last_lesson.date, schoolkid=schoolkid, subject=last_lesson.subject, teacher=last_lesson.teacher)
